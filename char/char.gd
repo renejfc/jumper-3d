779f44290f9e2942505e3fcae194967a8df2ade2
René <renejfc@gmail.com>
2024-06-05 18:44:01 +0200

    feat: add sprint

--- char/char.gd
+++ char/char.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
-const SPEED = 7.0
+const INITIAL_SPEED = 7.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY := 0.001
 
-var mouse_sensitivity := 0.001
+var current_speed := INITIAL_SPEED
+var sprinting_speed := INITIAL_SPEED * 2
+
 var twist_input := 0.0
 var pitch_input := 0.0
 
@@ -28,34 +31,39 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
   if event is InputEventMouseMotion:
     if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-      twist_input = -event.relative.x * mouse_sensitivity
-      pitch_input = -event.relative.y * mouse_sensitivity
+      twist_input = -event.relative.x * MOUSE_SENSITIVITY
+      pitch_input = -event.relative.y * MOUSE_SENSITIVITY
 
 # -------------------
 
 func char_movement(delta: float) -> void:
-    # Add the gravity.
-    if not is_on_floor():
-        velocity.y -= gravity * delta
+  if Input.is_action_pressed("sprint"):
+      current_speed = sprinting_speed
+  else:
+      current_speed = INITIAL_SPEED
 
-    # Handle jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+  # Add the gravity.
+  if not is_on_floor():
+      velocity.y -= gravity * delta
 
-    # Get the input direction and handle the movement/deceleration.
-    var input := Vector3.ZERO
-    input.x = Input.get_axis("move-left", "move-right")
-    input.z = Input.get_axis("move-forward", "move-back")
+  # Handle jump.
+  if Input.is_action_just_pressed("jump") and is_on_floor():
+      velocity.y = JUMP_VELOCITY
 
-    var direction = twist_pivot.basis * input
-    if direction:
-        velocity.x = direction.x * SPEED
-        velocity.z = direction.z * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
+  # Get the input direction and handle the movement/deceleration.
+  var input := Vector3.ZERO
+  input.x = Input.get_axis("move-left", "move-right")
+  input.z = Input.get_axis("move-forward", "move-back")
+  var direction = twist_pivot.basis * input
 
-    move_and_slide()
+  if direction:
+      velocity.x = direction.x * current_speed
+      velocity.z = direction.z * current_speed
+  else:
+      velocity.x = move_toward(velocity.x, 0, current_speed)
+      velocity.z = move_toward(velocity.z, 0, current_speed)
+
+  move_and_slide()
 
 # -------------------
 
